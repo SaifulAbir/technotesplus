97f57b6fa944736d2edec8587b963ad27206ed27
Saiful Islam <saiful.abir20@gmail.com>
2021-09-12 20:58:20 +0600

    Header component added

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -9,16 +9,18 @@ import {
 } from "./paths";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Note from "../pages/Note";
 
 class Routes extends Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path={LOGIN} component={Login} />
-        <Route exact path={SIGNUP} component={Signup} />
-      </Switch>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+                <Route exact path={LOGIN} component={Login} />
+                <Route exact path={SIGNUP} component={Signup} />
+                <ProtectedRoute exact path={NOTE} component={Note} />
+            </Switch>
+        );
+    }
 }
 
 export default Routes;
