2997310f524915493e95da440b849c95399757c3
Saiful Islam <saiful.abir20@gmail.com>
2021-09-14 17:28:17 +0600

    note share implemented in frontend

--- frontend/src/pages/Note/Note.js
+++ frontend/src/pages/Note/Note.js
@@ -6,7 +6,7 @@ import {
     Card,
     CardActions,
     CardContent,
-    CardHeader,
+    CardHeader, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
     Paper, TextField,
     Typography
 } from "@material-ui/core";
@@ -24,6 +24,7 @@ import IconButton from "@material-ui/core/IconButton";
 import {makeStyles} from "@material-ui/core/styles";
 import ConfirmDialog from "../../components/ConfirmDialog/ConfirmDialog";
 import {Autocomplete} from "@material-ui/lab";
+import {Form} from "../../components/Form/Form";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -38,8 +39,16 @@ export default function Note(props) {
     const [error, setError] = useState(null);
     const [noteRecord, setNoteRecord] = useState(null);
     const [noteList, setNoteList] = useState([]);
+    const [note, setNote] = useState('');
     const [previousTags, setPreviousTags] = useState([]);
     const [tagList, setTagList] = useState([]);
+    const [userList, setUserList] = useState([]);
+    const [user, setUser] = useState({});
+    const [openSharePopup, setOpenSharePopup] = React.useState(false);
+
+    const handleClose = () => {
+        setOpenSharePopup(false);
+    };
 
     const [notify, setNotify] = useState({
         isOpen: false,
@@ -137,7 +146,30 @@ export default function Note(props) {
         }
     };
 
+    var shareValues = {
+        note: note,
+        shared_with: user.id,
+    };
+
+    const handleShareSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await authApi.post("/api/share-note/", shareValues)
+                .then((resp) => {
+                    setOpenSharePopup(false);
+                    setNotify({
+                        isOpen: true,
+                        message: "Shared Successfully",
+                        type: "success",
+                    });
+                });
+        } catch (error) {
+            setError(error);
+        }
+    };
+
     const tagListUrl = "/api/tag-list";
+    const userListUrl = "/api/user-list";
 
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
@@ -146,6 +178,13 @@ export default function Note(props) {
             await api.get(tagListUrl).then((res) => {
                 if (res.status === 200) {
                     setTagList(res.data);
+                }
+            });
+        })();
+        (async () => {
+            await authApi.get(userListUrl).then((res) => {
+                if (res.status === 200) {
+                    setUserList(res.data);
                 }
             });
         })();
@@ -267,7 +306,11 @@ export default function Note(props) {
                                                 }}>
                                         <DeleteIcon />
                                     </IconButton>
-                                    <IconButton aria-label="share">
+                                    <IconButton aria-label="share" color="default"
+                                                onClick={() => {
+                                                    setOpenSharePopup(true);
+                                                    setNote(note.id);
+                                                }}>
                                         <ShareIcon />
                                     </IconButton>
                                     <IconButton color="primary"
@@ -283,6 +326,34 @@ export default function Note(props) {
                 </div>
 
             </Paper>
+            <Dialog open={openSharePopup} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Share Note</DialogTitle>
+                <Form onSubmit={handleShareSubmit}>
+                <DialogContent>
+                    <DialogContentText>
+                        To share note, please select user here.
+                    </DialogContentText>
+                    <Autocomplete
+                        id="combo-box-demo"
+                        options={userList}
+                        getOptionLabel={(option) => option.username}
+                        style={{ width: 400 }}
+                        renderInput={(params) => <TextField {...params} label="Choose user" variant="outlined" />}
+                        onChange={(event, value) => {
+                            setUser(value);
+                        }}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button type="submit" color="primary">
+                        Share
+                    </Button>
+                </DialogActions>
+                </Form>
+            </Dialog>
             <Popup
                 title="Note Form"
                 openPopup={openPopup}
