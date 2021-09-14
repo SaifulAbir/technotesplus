fef08ccdb34191115fe1215f7684b98d80d30580
Saiful Islam <saiful.abir20@gmail.com>
2021-09-15 05:50:25 +0600

    user serializer modified

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 
 class UserSerializer(ModelSerializer):
-
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
