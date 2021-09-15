1942e121848e15497ac9a6eb9944e37fb6e821cf
Saiful Islam <saiful.abir20@gmail.com>
2021-09-16 03:58:40 +0600

    default from email changed

--- technotesplus/settings.py
+++ technotesplus/settings.py
@@ -167,4 +167,4 @@ EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'saifulislamlk@gmail.com'
+DEFAULT_FROM_EMAIL = 'technotesplus@outlook.com'
